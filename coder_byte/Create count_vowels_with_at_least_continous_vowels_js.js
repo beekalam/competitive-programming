7610ee633d26be9ede687cff9c13199e5726d513
Mohammad Reza Mansouri <beekalam@gmail.com>
2018-07-20 12:14:32 +0430

    Update Create count_vowels_with_at_least_continous_vowels_js.js

--- coder_byte/Create count_vowels_with_at_least_continous_vowels_js.js
+++ coder_byte/Create count_vowels_with_at_least_continous_vowels_js.js
@@ -1,2 +1,14 @@
 function threeVowels(string){
+  let arr = string.split(' ');
+  let count =0;
+  
+  const pattern = /[aeiou]{3,}/gi;
+  
+  for(let word of arr){
+    if(word.match(pattern) !== null){
+      count+=1
+    }
+  }
+  
+  return count;
 }
