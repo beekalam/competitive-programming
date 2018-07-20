6bad50d7b9ae4c4bbca62a66615aef1e4f927e83
Mohammad Reza Mansouri <beekalam@gmail.com>
2018-07-20 13:48:56 +0430

    Update remove_all_adjacant_matching_characters.py

--- coder_byte/remove_all_adjacant_matching_characters.py
+++ coder_byte/remove_all_adjacant_matching_characters.py
@@ -4,6 +4,10 @@ def removePairs(string):
   
   while i < len(string):
     if i == len(string) - 1:
-      result += string[i];
-    else
-      
+      result += string[i]
+    elif string[i] != string[i+1]:
+      result += string[i]
+    else:
+      i += 1
+    i += 1
+ return result
